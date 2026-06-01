67ccee257bb07c69754f35a4219f9f4c61956752
km.mansi-at-794472626989 <km.mansi@msvgo.com>
2026-06-02 00:14:14 +0530

    created .env file

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://expenseUser:Expense%40123@expense-tracker-cluster.quxbqew.mongodb.net/?appName=expense-tracker-cluster"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.log("failed to connect database ❌", error.message);
